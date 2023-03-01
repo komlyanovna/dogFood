cab5a60149915366c39b039e785a0b58eefccbb0
Irina Gorshkova <88851629+komlyanovna@users.noreply.github.com>
2023-03-01 22:11:17 +0500

    Delete src directory

--- src/components/Routers/root.jsx
+++
@@ -1,7 +0,0 @@
-import { api } from '../customHookApi/CustomHookApi'
-// import { Outlet, Link } from 'react-router-dom'
-
-export async function loader() {
-  const apiToUser = await api()
-  return { apiToUser }
-}
